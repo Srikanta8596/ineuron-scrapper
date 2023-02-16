8f31c0c8ed81cf5368f248b681fbdec79338cd3d
Srikanta Biswal <srikantabiswal8093@gmail.com>
2023-02-16 16:24:08 +0530

    'aws'

--- database/configuration.py
+++ database/configuration.py
@@ -1,11 +1,12 @@
+import os
 #SAVE PDF FILES DIRECTORY
 PDF_DIRECTORY="/pdf_files"
 
 
 #MONGODB
-MONGO_DB_CONNECTION_STRING="mongodb+srv://srikanta:srikanta@data-science.kuyrwzp.mongodb.net/?retryWrites=true&w=majority"
-MONGO_DATA_BASE_NAME ="ineuron"
-MONGO_COLLECTION_NAME= "ineuron_courses"
+MONGO_DB_CONNECTION_STRING=os.environ('MONGO_DB_CONNECTION_STRING')
+MONGO_DATA_BASE_NAME =os.environ('MONGO_DATA_BASE_NAME')
+MONGO_COLLECTION_NAME= os.environ('MONGO_COLLECTION_NAME')
 
 #SQL
 SQL_HOST='localhost'
@@ -17,8 +18,8 @@ SQL_AUTH_PLUGIN='mysql_native_password'
 AUTO_COMMIT=True
 
 #AWS ACCESS KEY
-AWS_ACCESS_KEY_ID = 'AKIAZ5OFBRB2XMUOQ7UU'
-AWS_SECRET_ACCESS_KEY= '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'
+AWS_ACCESS_KEY_ID = os.environ('AWS_ACCESS_KEY_ID ')
+AWS_SECRET_ACCESS_KEY= os.environ('AWS_SECRET_ACCESS_KEY')
 
 #S3 BUCKET NAME
-S3_BUCKET_NAME = 'ineuron-scrapper-bucket'
+S3_BUCKET_NAME =  os.environ('S3_BUCKET_NAME')
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Ineuron Scrapper
+git branch -M main# Ineuron Scrapper
 This is a project which will be used to scrap  the website of the ineuron.ai website, this is the part FSDS class challenge.
 # Python Version Used
 python 3.9
